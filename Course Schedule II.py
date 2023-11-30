6ecc4a5ef42d7e3f63fb7092ece44469a7089fa0
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2023-11-29 16:20:03 -0800

    Update Course Schedule II.py

--- Course Schedule II.py
+++ Course Schedule II.py
@@ -18,23 +18,29 @@ class Solution:
 
         #defining DFS Function, which would be called recursively 
         def dfs(num):
-            # base case to check if the course is cyclic in graph
-            if num in cycle:
-                return False
             # base case to check if the course has already been visited
             if num in visited:
                 return True
+
+            # base case to check if the course is cyclic in graph
+            if num in cycle:
+                return False
+            
             #add the course to our current cycle
             cycle.add(num)
             for prereq in pre[num]:
+                #recursively call our traversal function for each preReq course
                 if dfs(prereq) == False:
                     return False
+            #removing course from cycle set as the cycle is completed
             cycle.remove(num)
+            #adding the course to visited set
             visited.add(num)
             result.append(num)
             return True
 
         for num in range(numCourses):
+            #checking if our tree (pre) has a cycle in it, if yes, returning empty result.
             if dfs(num) == False:
                 return []
             
