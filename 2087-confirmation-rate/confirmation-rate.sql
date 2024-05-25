cd359746fa317d9056eec4f47cef5798aba43af9
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2024-05-24 18:13:41 -0700

    Time: 1109 ms (87.52%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,8 +1,15 @@
-select user_id, 
-ROUND(SUM(if(action="confirmed",1,0))/count(user_id),2) as confirmation_rate
-from confirmations 
+-- select user_id, 
+-- ROUND(SUM(if(action="confirmed",1,0))/count(user_id),2) as confirmation_rate
+-- from confirmations 
+-- group by 1
+-- union 
+-- select user_id, 0.00 as confirmation_rate from signups where user_id not in (
+--     select distinct user_id from confirmations
+-- )
+
+select s.user_id, ROUND(AVG(if(c.action="confirmed",1,0)),2) as confirmation_rate
+from signups s
+left join 
+Confirmations c
+on s.user_id=c.user_id
 group by 1
-union 
-select user_id, 0.00 as confirmation_rate from signups where user_id not in (
-    select distinct user_id from confirmations
-)
