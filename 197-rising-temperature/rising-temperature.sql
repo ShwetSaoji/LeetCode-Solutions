e969044526deeb4888b6ad72bbd4d1ffe2a2e55e
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2024-06-11 19:35:17 -0700

    Time: 366 ms (98.12%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -6,7 +6,12 @@
 -- select id from CTE where prev_temp < temperature
 
 
+-- select w1.id from weather w1
+-- join weather w2
+-- on DATEDIFF(w1.recordDate, w2.recordDate) = 1
+-- and w1.temperature > w2.temperature
+
 select w1.id from weather w1
 join weather w2
-on DATEDIFF(w1.recordDate, w2.recordDate) = 1
-and w1.temperature > w2.temperature
+on DATE_SUB(w1.recordDate, INTERVAL 1 DAY) = w2.recordDate
+and  w1.temperature > w2.temperature
