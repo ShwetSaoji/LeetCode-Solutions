aa90f339d8cfc95c51fed98c47c802f69c900668
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2024-07-28 18:32:34 -0700

    Time: 890 ms (47.32%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,11 +1,30 @@
-select stu.student_id, stu.student_name,
-sub.subject_name,
-count(e.subject_name) as attended_exams from Students stu
-cross join 
-subjects sub
-left join 
-examinations e
-on stu.student_id = e.student_id
-and sub.subject_name = e.subject_name
-group by 1,3
-order by 1,3
+-- select stu.student_id, stu.student_name,
+-- sub.subject_name,
+-- count(e.subject_name) as attended_exams from Students stu
+-- cross join 
+-- subjects sub
+-- left join 
+-- examinations e
+-- on stu.student_id = e.student_id
+-- and sub.subject_name = e.subject_name
+-- group by 1,3
+-- order by 1,3
+
+
+
+
+
+
+
+
+
+
+
+
+select s.student_id, s.student_name, sub.subject_name,
+COUNT(e.subject_name) as attended_exams
+from students s cross join subjects sub
+left join examinations e
+on s.student_id = e.student_id and sub.subject_name = e.subject_name
+group by 1, 3
+order by 1, 3
