7af532dbe10b0d3ccbc8e0b7a4c0f26c3af8766b
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2024-07-29 10:54:18 -0700

    Time: 1175 ms (52.00%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -1,19 +1,33 @@
 
 
 
-(select u.name as results from users u
-inner join 
-movierating m
+-- (select u.name as results from users u
+-- inner join 
+-- movierating m
+-- on u.user_id = m.user_id 
+-- group by m.user_id
+-- order by count(m.user_id) desc, u.name
+-- limit 1)
+-- union all
+-- (select m.title as results from movies m 
+-- inner join 
+-- movierating mo
+-- on m.movie_id = mo.movie_id 
+-- where created_at like '2020-02%'
+-- group by mo.movie_id
+-- order by AVG(rating) desc, m.title
+-- limit 1)
+
+
+
+
+
+
+(select u.name as results  from users u join movierating m 
 on u.user_id = m.user_id 
-group by m.user_id
-order by count(m.user_id) desc, u.name
-limit 1)
+group by m.user_id order by count(m.user_id) DESC, u.name Limit 1)
 union all
 (select m.title as results from movies m 
-inner join 
-movierating mo
-on m.movie_id = mo.movie_id 
-where created_at like '2020-02%'
-group by mo.movie_id
-order by AVG(rating) desc, m.title
-limit 1)
+join movierating mr on m.movie_id = mr.movie_id 
+where SUBSTRING(mr.created_at, 1, 7) = "2020-02"
+group by mr.movie_id order by AVG(mr.rating) DESC, m.title Limit 1)
