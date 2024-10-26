e13e80f455247ebc7bc1d5b0099fd94079aef0bc
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2024-10-26 13:25:56 -0700

    Time: 11 ms (24.81%) | Memory: 20.3 MB (90.40%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,18 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        hashSet = {}
+
+        for i in nums:
+            if i in hashSet:
+                hashSet[i] += 1
+            else:
+                hashSet[i] = 1
+        
+        print(hashSet.items())
+        # arr = sorted(hashSet.items(), key = lambda it: it[1], reverse=True)
+        # arr = list(map(lambda it: it[0], arr))
+        # return arr[:k]
+
+        arr = dict(sorted(hashSet.items(), key = lambda x : x[1], reverse=True))
+        print(arr)
+        return list(arr.keys())[:k]
