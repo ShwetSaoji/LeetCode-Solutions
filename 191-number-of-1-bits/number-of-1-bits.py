8ed42f275b3c6d11bd1154576918f0e6f9a20f45
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2024-10-30 21:58:26 -0700

    Time: 0 ms (100.00%) | Memory: 16.6 MB (13.32%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -1,13 +1,10 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        arr = []
-        while n > 1:
-            arr.append(n%2)
-            n = n//2
         ans = 1
-        arr = arr[::-1]
-        for i in arr:
-            if i == 1:
+        while n > 1:
+            if n%2 == 1:
                 ans += 1
+            n = n//2
+        
         
         return ans
