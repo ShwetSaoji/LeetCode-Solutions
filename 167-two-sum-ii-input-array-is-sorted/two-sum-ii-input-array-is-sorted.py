ac7233af74b3633d00797d7e35a9c6ecaef619dd
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2024-11-07 17:45:42 -0800

    Time: 4 ms (52.01%) | Memory: 17.6 MB (74.11%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -1,15 +1,16 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        l, r = 0, len(numbers) -1
+        # ans = []
+        hmap = {}
 
-        while l < r:
-            diff = numbers[r] + numbers[l]
-            if diff == target:
-                return [l+1, r+1]
-            elif diff > target:
-                r -= 1
+        for i in range(len(numbers)):
+            diff = target - numbers[i]
+            if diff in hmap:
+                return [hmap[diff]+1, i+1]
             else:
-                l += 1
+                hmap[numbers[i]] = i
+        
+
         
         
         
