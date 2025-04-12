5c5dafa3ea08274924ecc6ac5a62e534177f983a
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-04-12 16:55:40 -0700

    Time: 551 ms (68.86%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,17 +1,19 @@
+-- select w1.id from Weather w1
+-- join 
+-- Weather w2
+-- on w1.id = (w2.id-1) and w1.temperature > w2.temperature
+
 -- with CTE as (
---     select *,
+--     select id, temperature, recordDate ,
+--     LAG(recordDate) OVER(ORDER BY recordDate) as prev_date,
 --     LAG(temperature) OVER(ORDER BY recordDate) as prev_temp
---     from weather
+--     from Weather
 -- )
--- select id from CTE where prev_temp < temperature
+-- select id from CTE where temperature > prev_temp and 
+-- DATEDIFF(recordDate, prev_date) = 1
 
-
--- select w1.id from weather w1
--- join weather w2
--- on DATEDIFF(w1.recordDate, w2.recordDate) = 1
--- and w1.temperature > w2.temperature
-
-select w1.id from weather w1
-join weather w2
-on DATE_SUB(w1.recordDate, INTERVAL 1 DAY) = w2.recordDate
-and  w1.temperature > w2.temperature
+select w1.id from Weather w1
+join 
+Weather w2
+on DATEDIFF(w1.recordDate, w2.recordDate) = 1 and
+w1.temperature > w2.temperature
