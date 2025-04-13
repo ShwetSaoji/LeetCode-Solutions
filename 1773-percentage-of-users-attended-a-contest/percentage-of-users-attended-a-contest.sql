63818fefef3ac79f5b06da56ceb34046aab6c1f5
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-04-13 16:14:29 -0700

    Time: 956 ms (85.34%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,5 +1,19 @@
-select contest_id, 
-ROUND(count(contest_id)*100/(select count(*) from users),2) as percentage
-from register
+-- select contest_id, 
+-- ROUND(count(contest_id)*100/(select count(*) from users),2) as percentage
+-- from register
+-- group by 1
+-- order by 2 desc, 1
+
+
+
+
+
+
+
+
+
+
+select contest_id, ROUND(count(user_id)*100/(select count(*) from Users),2) as percentage
+from Register 
 group by 1
-order by 2 desc, 1
+order by percentage desc, contest_id
