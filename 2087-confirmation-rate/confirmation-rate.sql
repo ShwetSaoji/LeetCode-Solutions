09448ae7f8a05754ed11dc731dc8459a9b2f3574
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-04-13 10:40:28 -0700

    Time: 622 ms (94.78%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -28,17 +28,48 @@
 
 
 
-with CTE as (
-select *, 
-COUNT(user_id) as tot_tr,
-SUM(CASE WHEN action="confirmed" THEN 1 ELSE 0 END) as cf_tr
-from confirmations 
-group by user_id
-)
+-- with CTE as (
+-- select *, 
+-- COUNT(user_id) as tot_tr,
+-- SUM(CASE WHEN action="confirmed" THEN 1 ELSE 0 END) as cf_tr
+-- from confirmations 
+-- group by user_id
+-- )
 
+-- select s.user_id, 
+-- ROUND(IFNULL(c.cf_tr/c.tot_tr, 0), 2) as confirmation_rate 
+-- from signups s 
+-- left join CTE c 
+-- on 
+-- s.user_id = c.user_id
+
+
+
+
+
+
+
+
+
+
+with CTE as (
+    select
+    user_id, 
+    SUM(CASE 
+    WHEN action = 'confirmed' THEN 1 else 0
+    END) as conf_count, 
+    count(*) as tot_trans
+    from confirmations
+    group by user_id
+)
 select s.user_id, 
-ROUND(IFNULL(c.cf_tr/c.tot_tr, 0), 2) as confirmation_rate 
-from signups s 
-left join CTE c 
-on 
-s.user_id = c.user_id
+ROUND(CASE 
+    WHEN (c.conf_count/c.tot_trans) is null THEN 0 
+    ELSE (c.conf_count/c.tot_trans)
+END, 2) as confirmation_rate 
+from 
+Signups s 
+left join 
+CTE c 
+on s.user_id = c.user_id
+
