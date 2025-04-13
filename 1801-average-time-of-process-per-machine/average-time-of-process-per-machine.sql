b36420462078169ab4ca77b11a5de535966c4510
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-04-12 17:22:25 -0700

    Time: 224 ms (88.63%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,10 +1,35 @@
-select a1.machine_id, ROUND(AVG(a2.timestamp-a1.timestamp),3) as processing_time
-from activity a1
+-- select a1.machine_id, ROUND(AVG(a2.timestamp-a1.timestamp),3) as processing_time
+-- from activity a1
+-- join 
+-- activity a2
+-- on a1.machine_id = a2.machine_id
+-- and
+-- a1.activity_type="start" 
+-- and
+-- a2.activity_type="end"
+-- group by a1.machine_id
+
+
+
+
+
+
+with CTE as (
+select a1.machine_id, a1.process_id, ROUND((a2.timestamp - a1.timestamp),3) as pro_time
+from Activity a1
 join 
-activity a2
-on a1.machine_id = a2.machine_id
-and
-a1.activity_type="start" 
-and
-a2.activity_type="end"
-group by a1.machine_id
+Activity a2 on
+a1.machine_id = a2.machine_id and
+a1.process_id = a2.process_id and
+a1.activity_type = "start" and
+a2.activity_type = "end"
+group by 1, 2
+)
+
+select machine_id, 
+ROUND(SUM(pro_time)/(select count(distinct process_id) from Activity), 3) as processing_time
+from CTE 
+group by 1
+
+
+
