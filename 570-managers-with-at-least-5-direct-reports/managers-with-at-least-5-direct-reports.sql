0d42299f33724764d3da0014504f32acffbcd30e
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-04-13 10:15:58 -0700

    Time: 333 ms (93.05%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,3 +1,22 @@
-# Write your MySQL query statement below
-select name from employee where id in (
-select managerId from employee group by 1 having count(*) >= 5)
+-- # Write your MySQL query statement below
+-- select name from employee where id in (
+-- select managerId from employee group by 1 having count(*) >= 5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+select name from Employee where id in (
+select managerId from Employee group by managerId having count(*)>=5)
