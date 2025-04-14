bd92d882b18b61a128e9aa4f4a8ed23d3b7ec987
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-04-14 15:48:12 -0700

    Time: 275 ms (79.43%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -1,3 +1,26 @@
-select *,
-CASE WHEN x + y > z AND y + z > x and z + x > y THEN "Yes" ELSE "No" END as triangle
-from triangle
+-- select *,
+-- CASE WHEN x + y > z AND y + z > x and z + x > y THEN "Yes" ELSE "No" END as triangle
+-- from triangle
+
+
+
+
+
+
+
+select x, y, z, 
+CASE 
+    WHEN x + y <= z THEN "No"
+    WHEN x + z <= y THEN "No" 
+    WHEN z + y <= x THEN "No"
+    ELSE "Yes"
+END as triangle
+from Triangle
+
+
+
+
+
+
+
+
