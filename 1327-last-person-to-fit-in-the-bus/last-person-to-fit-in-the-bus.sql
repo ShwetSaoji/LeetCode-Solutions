ca479e08d73fe2aa5d8595b98deb76f16bc385c5
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-04-14 16:35:22 -0700

    Time: 871 ms (77.78%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,8 +1,33 @@
+-- with CTE as (
+--     select person_name, 
+--     SUM(weight) OVER(order by turn) as cum_weight
+--     from queue
+-- )
+
+-- select person_name from CTE where cum_weight <= 1000
+-- order by cum_weight DESC limit 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 with CTE as (
-    select person_name, 
-    SUM(weight) OVER(order by turn) as cum_weight
-    from queue
+select person_name, 
+SUM(weight) OVER(ORDER BY turn) as tot_weight
+from Queue
 )
 
-select person_name from CTE where cum_weight <= 1000
-order by cum_weight DESC limit 1
+select person_name from CTE where tot_weight <= 1000
+order by tot_weight desc 
+limit 1
