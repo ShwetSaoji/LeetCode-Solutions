cd6edd178d9e5fe9795bc47f5ce8354d4da87002
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-04-14 09:31:21 -0700

    Time: 699 ms (60.94%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -1,13 +1,17 @@
--- select activity_date as day, COUNT(distinct user_id) as active_users from activity
--- where activity_date > "2019-06-27" and activity_date <= "2019-07-27"
+-- -- select activity_date as day, COUNT(distinct user_id) as active_users from activity
+-- -- where activity_date > "2019-06-27" and activity_date <= "2019-07-27"
+-- -- group by 1
+
+
+
+
+-- select activity_date as day, COUNT(distinct user_id) as active_users from activity 
+-- where activity_date <= "2019-07-27" and DATEDIFF("2019-07-27", activity_date) <= 30
 -- group by 1
 
 
 
 
-select activity_date as day, COUNT(distinct user_id) as active_users from activity 
-where activity_date <= "2019-07-27" and activity_date > DATE_SUB("2019-07-27", INTERVAL 30 DAY)
-group by 1
 
 
 
@@ -16,8 +20,8 @@ group by 1
 
 
 
-
-
-
-
-
+select activity_date as day, 
+count(distinct(user_id)) as active_users 
+from Activity
+where activity_date between "2019-06-28" and "2019-07-27"
+group by 1 
