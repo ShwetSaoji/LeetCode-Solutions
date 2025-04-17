7dd5c5aaa145088f5070270450e1c9171c04c942
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-04-16 17:09:03 -0700

    Time: 412 ms (63.03%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -1,16 +1,56 @@
 
+-- -- with CTE as (
+-- --     select *, 
+-- --     SUM(amount) as tot_day_amount
+-- --     from customer 
+-- --     group by visited_on
+-- -- )
+-- -- ,
+-- -- CTE2 as (
+-- --     select *,
+-- --     SUM(tot_day_amount) OVER(ORDER BY visited_on rows between 6 preceding and current row) as tot_amt
+-- --     from CTE
+-- -- )
+
+-- -- select visited_on, tot_amt as amount, ROUND(tot_amt/7,2) as average_amount from CTE2 
+-- -- where visited_on >= (select DATE_ADD(MIN(visited_on), INTERVAL 6 DAY) from customer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- with CTE as (
+-- select *, SUM(amount) as tot_amt
+-- from customer group by visited_on
+-- )
+-- select *, 
+-- SUM(tot_amt) OVER(ORDER BY visited_on rows between 6 preceding and current row) as rol_amt 
+-- from CTE where visited_on >= (select DATE_ADD(MIN(visited_on), INTERVAL 6 DAY) from customer)
+
+
+
+
+
 with CTE as (
-    select *, 
-    SUM(amount) as tot_day_amount
-    from customer 
-    group by visited_on
-)
-,
+select visited_on, SUM(amount) as tot_amt from Customer group by visited_on
+),
 CTE2 as (
-    select *,
-    SUM(tot_day_amount) OVER(ORDER BY visited_on rows between 6 preceding and current row) as tot_amt
+    select *, 
+    SUM(tot_amt) OVER(ORDER BY visited_on rows between 6 preceding and current row) as roll_amt
     from CTE
 )
-
-select visited_on, tot_amt as amount, ROUND(tot_amt/7,2) as average_amount from CTE2 
-where visited_on >= (select DATE_ADD(MIN(visited_on), INTERVAL 6 DAY) from customer)
+select visited_on, roll_amt as amount, 
+ROUND(roll_amt/7,2) as average_amount
+from CTE2
+where visited_on >= (select distinct(visited_on) from Customer where DATEDIFF(visited_on, (select MIN(visited_on) from Customer)) = 6)
+order by visited_on
