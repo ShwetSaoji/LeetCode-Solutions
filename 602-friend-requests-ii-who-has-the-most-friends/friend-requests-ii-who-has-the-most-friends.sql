1a996960abefe3417d75c3c64ff8b48dba8861a1
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-04-16 17:13:51 -0700

    Time: 330 ms (70.83%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,8 +1,33 @@
-with CTE as (select requester_id as id, count(requester_id) as num from RequestAccepted
-group by 1 
-union all
-select accepter_id as id, count(accepter_id) as num from RequestAccepted
-group by 1 )
+-- with CTE as (select requester_id as id, count(requester_id) as num from RequestAccepted
+-- group by 1 
+-- union all
+-- select accepter_id as id, count(accepter_id) as num from RequestAccepted
+-- group by 1 )
 
-select id, SUM(num) as num from CTE group by 1
-order by 2 desc limit 1
+-- select id, SUM(num) as num from CTE group by 1
+-- order by 2 desc limit 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+with CTE as (
+    select requester_id from RequestAccepted
+    UNION ALL
+    select accepter_id as requester_id from RequestAccepted
+)
+select requester_id as id, count(*) as num from CTE group by 1 order by count(*) desc limit 1
