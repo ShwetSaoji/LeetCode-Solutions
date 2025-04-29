ddf1bc9f8508d369c56b0f6483178a5fff43f921
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-04-29 14:59:08 -0700

    Time: 462 ms (94.33%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,3 +1,8 @@
 # Write your MySQL query statement below
-select player_id, MIN(event_date) as first_login from Activity
-group by 1
+
+with CTE as (
+select *, 
+RANK() OVER(PARTITION BY player_id ORDER BY event_date) as rnk 
+from Activity)
+
+select player_id, event_date as first_login from CTE where rnk= 1
