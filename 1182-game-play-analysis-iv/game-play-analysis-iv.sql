ec87c83c62a3e5f06e8851445c52b2b9de56de90
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-04-29 15:13:48 -0700

    Time: 583 ms (68.51%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,44 +1,15 @@
--- -- with CTE as (
--- -- select *, 
--- -- LAG(event_date) OVER(partition by player_id order by event_date) as prev_login,
--- -- RANK() OVER(partition by player_id order by event_date) as rnk
--- -- from activity 
--- -- )
-
--- -- select ROUND((select count(*) from CTE where rnk = 2 and DATEDIFF(event_date, prev_login) = 1)/
--- -- (select count(distinct player_id) from activity),2) as fraction
-
-
-
-
-
-
-
-
-
-
--- -- select ROUND((select count(*) from activity a1 join activity a2
--- -- on a1.player_id = a2.player_id and a1.event_date = DATE_SUB(a2.event_date, INTERVAL 1 DAY))/
--- -- (select count(distinct player_id) from activity),2) as fraction 
--- select count(*) from activity a1 join activity a2
--- on a1.player_id = a2.player_id and a1.event_date = DATE_SUB(a2.event_date, INTERVAL 1 DAY)
-
-
-
-
-
-
-
-
-
+# Write your MySQL query statement below
 
 with CTE as (
-select player_id, event_date,
-RANK() OVER(PARTITION BY player_id ORDER BY event_date) as rnk, 
-LAG(event_date) OVER(PARTITION BY player_id ORDER BY event_date) as prev_login
-from Activity
-)
+select *, 
+LEAD(event_date) OVER(PARTITION BY player_id ORDER BY event_date) as next_login,
+RANK() OVER(PARTITION BY player_id order by event_date) as rnk
+from Activity)
 
-select ROUND((select count(*) from CTE where rnk = 2 and
-DATEDIFF(event_date, prev_login) = 1)/(select count(distinct(player_id)) from Activity),2)
+select 
+ROUND((select count(distinct player_id) from CTE where DATEDIFF(next_login, event_date) = 1 and rnk=1)/
+(select count(distinct player_id) from Activity),2)
 as fraction
+
+
+-- select DATEDIFF('2025-05-02', '2023-05-01')
