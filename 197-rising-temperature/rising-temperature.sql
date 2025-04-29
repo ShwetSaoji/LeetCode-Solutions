d7d9f9d617fd756ba9fc59f48dbfb1def8d31aa0
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-04-29 11:30:20 -0700

    Time: 450 ms (83.33%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,19 +1,13 @@
--- select w1.id from Weather w1
--- join 
--- Weather w2
--- on w1.id = (w2.id-1) and w1.temperature > w2.temperature
+# Write your MySQL query statement below
 
 -- with CTE as (
---     select id, temperature, recordDate ,
---     LAG(recordDate) OVER(ORDER BY recordDate) as prev_date,
---     LAG(temperature) OVER(ORDER BY recordDate) as prev_temp
---     from Weather
--- )
--- select id from CTE where temperature > prev_temp and 
--- DATEDIFF(recordDate, prev_date) = 1
+-- select *, 
+-- LAG(temperature) OVER(ORDER BY recordDate) as prev_temp
+-- from Weather)
 
-select w1.id from Weather w1
-join 
-Weather w2
-on DATEDIFF(w1.recordDate, w2.recordDate) = 1 and
-w1.temperature > w2.temperature
+-- select id from CTE where temperature > prev_temp
+
+select w1.id from weather w1
+inner join weather w2 
+on w1.temperature > w2.temperature 
+and DATEDIFF(w1.recordDate, w2.recordDate) = 1
