23e7ba035eff94e64023c82cc0daedfbcb4b21d1
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-05-04 09:57:47 -0700

    Time: 318 ms (99.34%) | Memory: 0B (100.00%) - LeetSync

--- 1452-restaurant-growth/restaurant-growth.sql
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -1,56 +1,18 @@
+# Write your MySQL query statement below
 
--- -- with CTE as (
--- --     select *, 
--- --     SUM(amount) as tot_day_amount
--- --     from customer 
--- --     group by visited_on
--- -- )
--- -- ,
--- -- CTE2 as (
--- --     select *,
--- --     SUM(tot_day_amount) OVER(ORDER BY visited_on rows between 6 preceding and current row) as tot_amt
--- --     from CTE
--- -- )
-
--- -- select visited_on, tot_amt as amount, ROUND(tot_amt/7,2) as average_amount from CTE2 
--- -- where visited_on >= (select DATE_ADD(MIN(visited_on), INTERVAL 6 DAY) from customer)
+with sales as (
+select *, 
+SUM(amount) OVER(PARTITION BY visited_on) as day_sales,
+RANK() OVER(PARTITION BY visited_on ORDER BY customer_id) as rnk
+from Customer),
+week_sales as (
+select visited_on,
+SUM(day_sales) OVER(ORDER BY visited_on rows between 6 preceding and current row) as amount,
+ROUND(AVG(day_sales) OVER(ORDER BY visited_on rows between 6 preceding and current row),2) as average_amount
+from sales
+where rnk = 1 )
 
 
+select * from week_sales where DATEDIFF(visited_on, (select MIN(visited_on) from Customer)) >= 6
 
-
-
-
-
-
-
-
-
-
-
-
-
--- with CTE as (
--- select *, SUM(amount) as tot_amt
--- from customer group by visited_on
--- )
--- select *, 
--- SUM(tot_amt) OVER(ORDER BY visited_on rows between 6 preceding and current row) as rol_amt 
--- from CTE where visited_on >= (select DATE_ADD(MIN(visited_on), INTERVAL 6 DAY) from customer)
-
-
-
-
-
-with CTE as (
-select visited_on, SUM(amount) as tot_amt from Customer group by visited_on
-),
-CTE2 as (
-    select *, 
-    SUM(tot_amt) OVER(ORDER BY visited_on rows between 6 preceding and current row) as roll_amt
-    from CTE
-)
-select visited_on, roll_amt as amount, 
-ROUND(roll_amt/7,2) as average_amount
-from CTE2
-where visited_on >= (select distinct(visited_on) from Customer where DATEDIFF(visited_on, (select MIN(visited_on) from Customer)) = 6)
-order by visited_on
+-- select MIN(visited_on) from Customer
