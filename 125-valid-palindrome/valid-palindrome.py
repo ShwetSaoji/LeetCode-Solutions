26b7955447dc4a536019fc16b638ab515e94a814
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-05-03 22:10:13 -0700

    Time: 7 ms (81.04%) | Memory: 18 MB (83.43%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,24 +1,17 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # l , r = 0 , len(s)-1
+        l, r = 0, len(s)-1
 
-        # while l < r:
-        #     while not s[l].isalpha() and l < r:
-        #         l += 1
-        #     while not s[r].isalpha() and r > l:
-        #         r -= 1
-            
-        #     if s[l].lower() == s[r].lower():
-        #         l += 1
-        #         r -= 1
-        #     else:
-        #         return False
+        while l < r:
+            while not s[l].isalnum() and l < r:
+                l += 1
+            while not s[r].isalnum() and l < r:
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            else:
+                l += 1
+                r -= 1
+
         
-        # return True
-
-        string = ""
-        for i in s:
-            if i.isalnum():
-                print(i)
-                string += i.lower()
-        return string == string[::-1]
+        return True
