8a3feb102e0792965edd293f4617f29fc365e5df
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-05-04 15:53:41 -0700

    Time: 11 ms (17.36%) | Memory: 18.1 MB (37.95%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,12 +1,12 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        hms = {}
-        hmt = {}
-        for i in range(len(s)):
-            if (s[i] in hms and hms[s[i]] != t[i]) or (t[i] in hmt and hmt[t[i]] != s[i]):
-                return False
-            hms[s[i]] = t[i]
-            hmt[t[i]] = s[i]
-        return True
+        hm_s = {}
+        hm_t = {}
 
-        
+        for i in range(len(s)):
+            if (s[i] in hm_s and hm_s[s[i]] != t[i])  or (t[i] in hm_t and hm_t[t[i]] != s[i]):
+                return False
+            hm_s[s[i]] = t[i]
+            hm_t[t[i]] = s[i]
+
+        return True
