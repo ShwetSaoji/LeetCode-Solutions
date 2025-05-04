1a0ea82c4c9efac29cd919883fdf406ceb4a38c8
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-05-04 15:42:05 -0700

    Time: 7 ms (57.20%) | Memory: 19.4 MB (51.76%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,13 +1,10 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hp = {}
+        hm = defaultdict(int)
 
         for i in nums:
-            if i in hp:
-                hp[i] += 1
-            else:
-                hp[i] = 1
+            hm[i] += 1
         
-        for item, value in hp.items():
-            if value > len(nums)//2:
-                return item
+        for i , v in hm.items():
+            if v > (len(nums)/2):
+                return i 
