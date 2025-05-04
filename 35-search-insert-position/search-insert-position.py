e9ee1f9a83921c9db7674ca13f4ad228f708c2bb
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-05-03 17:04:24 -0700

    Time: 0 ms (100.00%) | Memory: 18.6 MB (26.77%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -3,11 +3,12 @@ class Solution:
         l, r = 0 , len(nums)-1
 
         while l <= r:
-            mid = (l+r)//2
-            if target == nums[mid]:
+            mid = (r+l)//2
+            if nums[mid] == target:
                 return mid
-            elif target > nums[mid]:
-                l = mid + 1
-            else:
+            elif nums[mid] > target:
                 r = mid - 1
-        return l
+            else:
+                l = mid + 1
+        
+        return l 
