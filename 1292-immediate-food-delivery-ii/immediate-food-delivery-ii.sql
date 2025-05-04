4dea28fa2e27763e4b9c20dec0940b1c05185796
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-05-04 16:13:56 -0700

    Time: 621 ms (79.21%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,49 +1,11 @@
--- -- with CTE as (select *, 
--- -- RANK() OVER(partition by customer_id order by order_date) as rnk
--- -- from 
--- -- delivery)
-
--- -- select ROUND((select count(*) from CTE where order_date = customer_pref_delivery_date and rnk=1)*100/
--- -- (select count(distinct customer_id) from delivery),2) as immediate_percentage
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- with CTE as (
--- select *, 
--- RANK() OVER(PARTITION BY customer_id ORDER BY order_date) as rnk,
--- CASE WHEN customer_pref_delivery_date = order_date THEN "immediate" ELSE "SC" END as o_type
--- from delivery
--- )
--- select (select count(*) from CTE where rnk=1 and o_type="immediate")*100/(select count(*) from CTE where rnk = 1) as immediate_percentage
-
-
-
-
-
-
-
-
-with CTE AS(
-select order_date, customer_pref_delivery_date, 
-RANK() OVER(PARTITION BY customer_id ORDER BY order_date) as rnk 
+# Write your MySQL query statement below
+with CTE as (
+select *, 
+RANK() OVER(PARTITION BY customer_id ORDER BY order_date) as rnk,
+CASE 
+    WHEN order_date = customer_pref_delivery_date THEN 'immediate' ELSE 'scheduled'
+END as order_type
 from Delivery)
 
-select ROUND((select count(*) from CTE where rnk = 1 and order_date = customer_pref_delivery_date)*100/
-(select count(distinct(customer_id)) from Delivery),2) as immediate_percentage
+select ROUND((select count(1) from CTE where rnk = 1 and order_type = 'immediate')*100/
+(select count(1) from CTE where rnk = 1),2) as immediate_percentage
