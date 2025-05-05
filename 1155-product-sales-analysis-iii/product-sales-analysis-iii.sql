885fddf6d9ae67402025f937ed62bcc2aa55a10f
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-05-04 20:13:35 -0700

    Time: 952 ms (89.28%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,24 +1,8 @@
--- # Write your MySQL query statement below
--- select product_id, year as first_year, quantity, price
--- from sales
--- where (product_id, year) in (
---     select product_id, MIN(year) from sales
---     group by 1
--- )
+# Write your MySQL query statement below
 
 
-
-
-
-
-
-
-with CTE as (
-    select *, 
-    RANK() OVER(PARTITION BY product_id ORDER BY year) as rnk
-    from Sales
-)
-
-select c.product_id, c.year as first_year, c.quantity, c.price 
-from CTE c 
-where c.rnk = 1
+select product_id, year as first_year, quantity, price from (
+select *, 
+RANK() OVER(PARTITION BY product_id ORDER BY year) as rnk 
+from Sales) A
+where rnk = 1
