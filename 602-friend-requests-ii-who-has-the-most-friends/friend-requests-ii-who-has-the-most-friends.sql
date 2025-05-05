5c25e1e53fb4eccf267778aeb1604ec03c2db503
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-05-04 20:23:57 -0700

    Time: 305 ms (81.72%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,33 +1,11 @@
--- with CTE as (select requester_id as id, count(requester_id) as num from RequestAccepted
--- group by 1 
--- union all
--- select accepter_id as id, count(accepter_id) as num from RequestAccepted
--- group by 1 )
-
--- select id, SUM(num) as num from CTE group by 1
--- order by 2 desc limit 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Write your MySQL query statement below
 with CTE as (
-    select requester_id from RequestAccepted
-    UNION ALL
-    select accepter_id as requester_id from RequestAccepted
-)
-select requester_id as id, count(*) as num from CTE group by 1 order by count(*) desc limit 1
+select requester_id as id from RequestAccepted
+UNION ALL
+select accepter_id as id from RequestAccepted)
+
+
+select id, COUNT(1) as num from CTE 
+group by id 
+order by num desc 
+LIMIT 1
