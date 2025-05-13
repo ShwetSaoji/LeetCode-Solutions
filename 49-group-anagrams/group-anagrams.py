d9ce5870ccd2b7765ef39eac5d56476d295cadd6
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-05-13 09:40:49 -0700

    Time: 19 ms (37.00%) | Memory: 20.8 MB (62.82%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,19 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hashset = defaultdict(list)
+        hashMap = {}
 
-        # for i in strs:
-        #     sort_str = ''.join(sorted(i))
-        #     if sort_str in hashset:
-        #         hashset[sort_str].append(i)
-        #     else:
-        #         hashset[sort_str] = [i]
-
-        for s in strs:
-            chars = [0] * 26
-            for c in s:
-                chars[ord(c) - ord("a")] += 1
-
-            hashset[tuple(chars)].append(s)    
-
-        return list(hashset.values())
+        for i in strs:
+            si = str(sorted(i))
+            if si in hashMap:
+                hashMap[si].append(i)
+            else:
+                hashMap[si] = [i]
+        
+        return list(hashMap.values())
