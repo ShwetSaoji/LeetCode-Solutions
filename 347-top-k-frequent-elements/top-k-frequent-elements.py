f01e6cdc46f9e703c5e1de5baf630caf3a083943
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-05-13 09:50:13 -0700

    Time: 3 ms (90.98%) | Memory: 21 MB (96.59%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,33 +1,17 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # hashSet = {}
+        hashMap = {}
 
-        # for i in nums:
-        #     if i in hashSet:
-        #         hashSet[i] += 1
-        #     else:
-        #         hashSet[i] = 1
-        
-        # print(hashSet.items())
-        # # arr = sorted(hashSet.items(), key = lambda it: it[1], reverse=True)
-        # # arr = list(map(lambda it: it[0], arr))
-        # # return arr[:k]
-
-        # arr = dict(sorted(hashSet.items(), key = lambda x : x[1], reverse=True))
-        # print(arr)
-        # return list(arr.keys())[:k]
-
-        hashSet = {}
-        res = []
         for i in nums:
-            if i in hashSet:
-                hashSet[i] += 1
+            hashMap[i] = hashMap.get(i, 0) + 1
+        
+        hashMap = dict(sorted(hashMap.items(), key=lambda i:i[1], reverse=True))
+
+        ans = []
+        for i, v in hashMap.items():
+            if k <= 0:
+                break
             else:
-                hashSet[i] = 1
-        
-        for key, value in hashSet.items():
-            heapq.heappush(res, [value, key])
-            if len(res) > k:
-                heapq.heappop(res)
-        
-        return [x for _, x in res]
+                ans.append(i)
+                k -= 1
+        return ans 
