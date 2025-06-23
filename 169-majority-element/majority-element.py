9529b8a1f23b970271d45aca0c1e3dffbf2aa781
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-06-23 13:31:13 -0500

    Time: 11 ms (29.41%) | Memory: 19.4 MB (19.56%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,10 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hm = defaultdict(int)
+        if len(nums) == 1:
+            return nums[0]
+        maj = len(nums)/2
+
+        hashMap = {}
 
         for i in nums:
-            hm[i] += 1
-        
-        for i , v in hm.items():
-            if v > (len(nums)/2):
-                return i 
+            if i in hashMap:
+                hashMap[i] += 1
+                if hashMap[i] > maj:
+                    return i
+            else:
+                hashMap[i] = 1
