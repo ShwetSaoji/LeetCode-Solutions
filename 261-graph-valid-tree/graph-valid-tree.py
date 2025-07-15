8249aa84d888dcfb2caaac0e63db62918b739ee1
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-07-15 13:50:32 -0500

    Time: 0 ms (100.00%) | Memory: 18.9 MB (67.43%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.py
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -1,31 +1,27 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if len(edges) > (n - 1):
+        if n - len(edges) != 1:
             return False
-
-        visit = set()
-        nei = [ [] for _ in range(n)]
-
-        for e1, e2 in edges:
-            nei[e1].append(e2)
-            nei[e2].append(e1)
         
+        visit = set()
+
+        hashmap = {i:[] for i in range(n)}
+
+        for node, nei in edges:
+            hashmap[node].append(nei)
+            hashmap[nei].append(node)
+
         def dfs(node, prev):
             if node in visit:
                 return False
-            # if nei[node] == []:
-            #     return True
             visit.add(node)
-            for i in nei[node]:
-                if i == prev:
+            for nei in hashmap[node]:
+                if nei == prev:
                     continue
-                else:
-                    if not dfs(i, node): return False
+                if not dfs(nei, node):
+                    return False
             return True
-            
-        # if not dfs(0, -1): return False
-        # if len(visit) == n: 
-        #     return True
-        # else:
-        #     return False
-        return dfs(0, -1) and len(visit) == n
+                
+        if not dfs(0, -1):
+            return False
+        return len(visit) == n
