c967a78027c5810653aed37686fb7537089c2002
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-09-25 22:38:18 -0500

    Time: 322 ms (92.03%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -12,7 +12,38 @@
 
 
 
+-- select query_name, ROUND(AVG(rating/position),2) as quality, 
+-- ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END)*100/count(query_name),2) as poor_query_percentage
+-- from Queries 
+-- group by 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 select query_name, ROUND(AVG(rating/position),2) as quality, 
-ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END)*100/count(query_name),2) as poor_query_percentage
-from Queries 
+ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END)*100/COUNT(query_name),2) as poor_query_percentage
+from Queries
 group by 1
