13a009592328c109c565a3ad7f2403b2c35470ca
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-09-25 23:10:35 -0500

    Time: 475 ms (99.40%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -1,10 +1,73 @@
-# Write your MySQL query statement below
+-- -- with CTE as (
+-- -- select *, 
+-- -- LAG(event_date) OVER(partition by player_id order by event_date) as prev_login,
+-- -- RANK() OVER(partition by player_id order by event_date) as rnk
+-- -- from activity 
+-- -- )
+
+-- -- select ROUND((select count(*) from CTE where rnk = 2 and DATEDIFF(event_date, prev_login) = 1)/
+-- -- (select count(distinct player_id) from activity),2) as fraction
+
+
+
+
+
+
+
+
+
+
+-- -- select ROUND((select count(*) from activity a1 join activity a2
+-- -- on a1.player_id = a2.player_id and a1.event_date = DATE_SUB(a2.event_date, INTERVAL 1 DAY))/
+-- -- (select count(distinct player_id) from activity),2) as fraction 
+-- select count(*) from activity a1 join activity a2
+-- on a1.player_id = a2.player_id and a1.event_date = DATE_SUB(a2.event_date, INTERVAL 1 DAY)
+
+
+
+
+
+
+
+
+
+
+-- with CTE as (
+-- select player_id, event_date,
+-- RANK() OVER(PARTITION BY player_id ORDER BY event_date) as rnk, 
+-- LAG(event_date) OVER(PARTITION BY player_id ORDER BY event_date) as prev_login
+-- from Activity
+-- )
+
+-- select ROUND((select count(*) from CTE where rnk = 2 and
+-- DATEDIFF(event_date, prev_login) = 1)/(select count(distinct(player_id)) from Activity),2)
+-- as fraction
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 with CTE as (
 select *, 
-RANK() OVER(PARTITION BY player_id ORDER BY event_date) as rnk,
+RANK() OVER(PARTITION BY player_id ORDER BY event_date) as rnk, 
 LEAD(event_date) OVER(PARTITION BY player_id ORDER BY event_date) as next_login
-from Activity)
+from Activity
+)
 
-select ROUND((select COUNT(*) from CTE where rnk = 1 and DATEDIFF(next_login, event_date) = 1)/
-(select COUNT(DISTINCT player_id) from Activity),2) as fraction 
+select 
+ROUND((select count(*) from CTE where rnk = 1 and DATEDIFF(next_login, event_date) = 1)/
+(select count(distinct player_id) from CTE),2) as fraction
+
