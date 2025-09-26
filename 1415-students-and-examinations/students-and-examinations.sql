6e1024bde020d1c884bb241291b02bc9c2c8e92c
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-09-25 19:28:16 -0500

    Time: 822 ms (93.95%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,40 +1,64 @@
--- select stu.student_id, stu.student_name,
--- sub.subject_name,
--- count(e.subject_name) as attended_exams from Students stu
+-- -- select stu.student_id, stu.student_name,
+-- -- sub.subject_name,
+-- -- count(e.subject_name) as attended_exams from Students stu
+-- -- cross join 
+-- -- subjects sub
+-- -- left join 
+-- -- examinations e
+-- -- on stu.student_id = e.student_id
+-- -- and sub.subject_name = e.subject_name
+-- -- group by 1,3
+-- -- order by 1,3
+
+
+
+
+
+
+
+
+
+
+-- select stu.student_id, stu.student_name, sub.subject_name,
+-- count(e.student_id) as attended_exams
+-- from students stu
 -- cross join 
 -- subjects sub
 -- left join 
 -- examinations e
--- on stu.student_id = e.student_id
--- and sub.subject_name = e.subject_name
--- group by 1,3
--- order by 1,3
-
-
-
-
-
-
-
-
-
-
-select stu.student_id, stu.student_name, sub.subject_name,
-count(e.student_id) as attended_exams
-from students stu
-cross join 
-subjects sub
-left join 
-examinations e
-on stu.student_id = e.student_id and
-sub.subject_name = e.subject_name
-group by 1, 3
-order by 1, 3
-
--- select s.student_id, s.student_name, sub.subject_name,
--- COUNT(e.subject_name) as attended_exams
--- from students s cross join subjects sub
--- left join examinations e
--- on s.student_id = e.student_id and sub.subject_name = e.subject_name
+-- on stu.student_id = e.student_id and
+-- sub.subject_name = e.subject_name
 -- group by 1, 3
 -- order by 1, 3
+
+-- -- select s.student_id, s.student_name, sub.subject_name,
+-- -- COUNT(e.subject_name) as attended_exams
+-- -- from students s cross join subjects sub
+-- -- left join examinations e
+-- -- on s.student_id = e.student_id and sub.subject_name = e.subject_name
+-- -- group by 1, 3
+-- -- order by 1, 3
+
+
+
+
+
+
+
+
+with CTE as (
+select stu.student_id, stu.student_name, sub.subject_name 
+from Students stu 
+cross join 
+Subjects sub)
+
+select c.student_id, c.student_name, c.subject_name,  COUNT(e.student_id) as attended_exams
+from CTE c
+left join 
+Examinations e
+on c.student_id = e.student_id
+and c.subject_name = e.subject_name
+group by 1, 3 
+order by 1, 3 
+
+
