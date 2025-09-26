1acfbf4540cc472708c17602b07564881d30f399
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-09-26 01:29:37 -0500

    Time: 2162 ms (7.74%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,33 +1,42 @@
-# Write your MySQL query statement below
+-- with CTE as (
+--     select 
+--     CASE
+--     WHEN income < 20000 THEN "Low Salary"
+--     WHEN income >= 20000 and income <= 50000 THEN "Average Salary"
+--     ELSE "High Salary" 
+--     END 
+--     as category
+--     from 
+--     accounts
+-- )
+
+-- select category, COUNT(category)  as accounts_count from CTE group by 1 
+-- -- select * from CTE
+
+select 'Low Salary' as category, COUNT(account_id) as accounts_count 
+from accounts where income  < 20000
+union all
+select 'Average Salary' as category, COUNT(account_id) as accounts_count 
+from accounts where income  >= 20000 and income <= 50000
+union all
+select 'High Salary' as category, COUNT(account_id) as accounts_count 
+from accounts where income>50000
+
+
+
+
+
+
+
+
+
+
+
+
 -- select 
--- 'Low Salary' as category,
--- SUM(CASE
---     WHEN income < 20000 THEN 1 ELSE 0 
--- END) as 
-
-with CTE as (
-select  
-CASE
-    WHEN income < 20000 THEN 'Low Salary'
-    WHEN income >= 20000 and income <= 50000 THEN 'Average Salary'
-    ELSE 'High Salary'
-END as categories 
-from Accounts)
-
-select 'Low Salary' as category, 
-SUM(CASE 
-    WHEN categories = 'Low Salary' THEN 1 ELSE 0 
-END) as accounts_count 
-from CTE
-UNION ALL
-select 'Average Salary' as category, 
-SUM(CASE 
-    WHEN categories = 'Average Salary' THEN 1 ELSE 0 
-END) as accounts_count 
-from CTE
-UNION ALL
-select 'High Salary' as category, 
-SUM(CASE 
-    WHEN categories = 'High Salary' THEN 1 ELSE 0 
-END) as accounts_count 
-from CTE
+-- CASE 
+--     WHEN income < 20000 THEN "Low Salary"
+--     WHEN income <= 50000 THEN "Average Salary"
+--     ELSE "High Salary"
+-- END as category
+-- from Accounts
