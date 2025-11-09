2ce6e6178504c90ffdb230bd4b188eceea3387f7
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-11-09 14:03:59 -0600

    Time: 455 ms (61.26%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -64,13 +64,33 @@
 
 
 
-select product_id, 10 as price from Products
-group by 1
-having MIN(change_date) > '2019-08-16'
-union
-select distinct product_id, new_price as price from Products 
-where (product_id, change_date) in (
-    select product_id, MAX(change_date) from Products 
-    where change_date <= '2019-08-16'
-    group by 1
+-- select product_id, 10 as price from Products
+-- group by 1
+-- having MIN(change_date) > '2019-08-16'
+-- union
+-- select distinct product_id, new_price as price from Products 
+-- where (product_id, change_date) in (
+--     select product_id, MAX(change_date) from Products 
+--     where change_date <= '2019-08-16'
+--     group by 1
+-- )
+
+
+
+
+
+
+
+
+
+
+
+select product_id, 10 as price
+from Products group by 1 having 
+MIN(change_date) > '2019-08-16'
+union all
+select product_id, new_price as price 
+from Products where (product_id, change_date) in (
+    select product_id, MAX(change_date) from Products where change_date <= '2019-08-16'
+    group by 1 
 )
