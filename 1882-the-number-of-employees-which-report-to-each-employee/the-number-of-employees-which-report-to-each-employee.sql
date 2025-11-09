8f2ee96fe24cff15597b060c3af35267fe45140d
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-11-09 12:02:22 -0600

    Time: 671 ms (26.53%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -47,16 +47,27 @@
 
 
 
-with CTE as (
-select reports_to, COUNT(reports_to) as reports_count, ROUND(AVG(age)) as average_age
-from Employees e 
-where reports_to is not null
-group by 1
-)
+-- with CTE as (
+-- select reports_to, COUNT(reports_to) as reports_count, ROUND(AVG(age)) as average_age
+-- from Employees e 
+-- where reports_to is not null
+-- group by 1
+-- )
 
-select c.reports_to as employee_id, e.name, c.reports_count, c.average_age 
-from CTE c 
-inner join 
-EMployees e 
-on c.reports_to = e.employee_id
+-- select c.reports_to as employee_id, e.name, c.reports_count, c.average_age 
+-- from CTE c 
+-- inner join 
+-- EMployees e 
+-- on c.reports_to = e.employee_id
+-- order by 1
+
+
+
+
+
+select mgr.employee_id, mgr.name, COUNT(emp.employee_id) as reports_count, ROUND(AVG(emp.age)) as average_age
+from employees emp 
+join employees mgr
+on emp.reports_to = mgr.employee_id
+group by 1
 order by 1
