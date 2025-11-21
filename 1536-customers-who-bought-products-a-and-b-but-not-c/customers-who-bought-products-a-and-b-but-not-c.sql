9d0000e21e5e839a80a39f53e7362e379fa7fc5b
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-11-21 13:57:47 -0600

    Time: 634 ms (62.74%) | Memory: 0B (100.00%) - LeetSync

--- 1536-customers-who-bought-products-a-and-b-but-not-c/customers-who-bought-products-a-and-b-but-not-c.sql
+++ 1536-customers-who-bought-products-a-and-b-but-not-c/customers-who-bought-products-a-and-b-but-not-c.sql
@@ -1,14 +1,11 @@
 # Write your MySQL query statement below
-
-with CTE as (
-select distinct o1.customer_id from Orders o1
-inner join orders o2 
-on o1.customer_id = o2.customer_id and 
-o1.product_name = 'A' and 
-o2.product_name = 'B')
-
-select c.customer_id, cus.customer_name from CTE c 
-inner join 
-Customers cus 
-on c.customer_id = cus.customer_id 
-where c.customer_id not in (select customer_id from Orders where product_name = 'C')
+select c.* from Customers c
+join 
+Orders o 
+on c.customer_id = o.customer_id
+where o.product_name in ('A', 'B') and 
+c.customer_id not in (
+    select customer_id from Orders where product_name = 'C'
+)
+group by c.customer_name having count(distinct o.product_name) > 1
+order by c.customer_id
