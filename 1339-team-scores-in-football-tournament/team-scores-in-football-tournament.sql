812361451bff3a5df61483cfbb9b4af7cba9dba9
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-11-22 14:14:21 -0600

    Time: 784 ms (30.81%) | Memory: 0B (100.00%) - LeetSync

--- 1339-team-scores-in-football-tournament/team-scores-in-football-tournament.sql
+++ 1339-team-scores-in-football-tournament/team-scores-in-football-tournament.sql
@@ -1,23 +1,26 @@
 # Write your MySQL query statement below
-with CTE as (
-select team_id, SUM(points) as tot_points from (
+with CTE as (select team_id, SUM(points) as num_points from (
 select host_team as team_id, 
-CASE WHEN host_goals > guest_goals THEN 3 
-WHEN guest_goals > host_goals THEN 0 
-ELSE 1 END as points
+CASE
+WHEN host_goals > guest_goals THEN 3
+WHEN host_goals < guest_goals THEN 0 
+ELSE 1
+END as points 
 from Matches
-UNION ALL
+union all
 select guest_team as team_id, 
-CASE WHEN guest_goals > host_goals THEN 3 
-WHEN host_goals > guest_goals THEN 0 
-ELSE 1 END as points
+CASE
+WHEN guest_goals > host_goals THEN 3
+WHEN guest_goals < host_goals THEN 0 
+ELSE 1
+END as points 
 from Matches) a
-group by 1
-)
+group by team_id)
 
-select t.team_id, t.team_name, IFNULL(c.tot_points,0) as num_points 
-from Teams t 
+select t.*, 
+IFNULL(c.num_points,0) as num_points from 
+Teams t 
 left join 
 CTE c 
-on t.team_id = c.team_id
-order by 3 desc, 1
+on t.team_id = c.team_id 
+order by 3 desc , 1
