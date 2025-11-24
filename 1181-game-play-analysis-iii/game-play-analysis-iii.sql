6305823388d7c5ea59fbe284a9f0e95da0007edd
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-11-23 18:22:22 -0600

    Time: 655 ms (35.89%) | Memory: 0B (100.00%) - LeetSync

--- 1181-game-play-analysis-iii/game-play-analysis-iii.sql
+++ 1181-game-play-analysis-iii/game-play-analysis-iii.sql
@@ -1,7 +1,4 @@
 # Write your MySQL query statement below
-
-
-select player_id, event_date, 
-SUM(games_played) OVER(PARTITION BY player_id ORDER BY event_date) as games_played_so_far
+select player_id, event_date,
+SUM(games_played) OVER(PARTITION BY player_id ORDER by event_date) as games_played_so_far
 from Activity
-
