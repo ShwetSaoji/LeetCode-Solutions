45a76c2fbddcb1e4f1acd92f7808a8f05a2854db
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-11-23 18:44:58 -0600

    Time: 471 ms (80.46%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1898-leetflex-banned-accounts/leetflex-banned-accounts.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+select distinct l1.account_id from LogInfo l1
+inner join 
+LogInfo l2
+on l1.account_id = l2.account_id 
+and l1.ip_address != l2.ip_address
+and l2.login between l1.login and l1.logout
