f0ab69c8787f8fe1b48161148f26838d7107b1d7
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-11-23 18:05:09 -0600

    Time: 572 ms (24.06%) | Memory: 0B (100.00%) - LeetSync

--- 1399-page-recommendations/page-recommendations.sql
+++ 1399-page-recommendations/page-recommendations.sql
@@ -1,9 +1,13 @@
 # Write your MySQL query statement below
+-- select distinct page_id  as recommended_page from likes where 
+-- user_id in (select user2_id from Friendship where user1_id = 1 
+-- union 
+-- select user1_id from Friendship where user2_id = 1 
+-- ) and user_id != 1
 
-select distinct page_id as recommended_page from Likes where user_id in (
-select user2_id from Friendship where user1_id = '1' 
-UNION ALL 
-select user1_id from Friendship where user2_id = '1'
-) and page_id not in (
-    select page_id from Likes where user_id = '1'
-)
+select distinct page_id recommended_page 
+from likes 
+where page_id not in(select page_id from likes where user_id =1)
+and user_id in
+(   select user1_id from friendship where user2_id =1
+union     select user2_id from friendship where user1_id =1            )
