9c1b4dda7093d8cc2463bbba60180e63ca031057
Shwet Saoji <112597837+ShwetSaoji@users.noreply.github.com>
2025-11-23 18:56:34 -0600

    Time: 510 ms (34.07%) | Memory: 0B (100.00%) - LeetSync

--- 1180-game-play-analysis-ii/game-play-analysis-ii.sql
+++ 1180-game-play-analysis-ii/game-play-analysis-ii.sql
@@ -1,9 +1,7 @@
 # Write your MySQL query statement below
 
-
-with CTE as (
-select *, 
+with CTE as (select player_id, device_id, 
 RANK() OVER(PARTITION BY player_id ORDER BY event_date) as rnk 
 from Activity)
 
-select player_id, device_id  from CTE where rnk= 1
+select player_id, device_id from CTE where rnk = 1
